d8bc2d01b166b23bca771e586c2255c799497e45
hyeong08 <gudtjq9735@gmail.com>
2023-07-07 12:54:05 +0900

    style: users dotenv require add

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const User = require('../schemas/user');
 const jwt = require('jsonwebtoken')
+require('dotenv').config();
 
 // 회원가입 API
 router.post('/signup', async (req, res) => {
